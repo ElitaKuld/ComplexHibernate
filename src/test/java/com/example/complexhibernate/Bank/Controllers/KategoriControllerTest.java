5d8549b7b75aa8c9ab586fa2994efea396916f86
ElitaKuld <elitakuld@gmail.com>
2023-04-25 14:27:07 +0200

    Skrivit tester för KategoriController

--- src/test/java/com/example/complexhibernate/Bank/Controllers/KategoriControllerTest.java
+++ src/test/java/com/example/complexhibernate/Bank/Controllers/KategoriControllerTest.java
@@ -32,7 +32,7 @@ class KategoriControllerTest {
     private KategoriRepo mockRepo;
 
     @BeforeEach
-    public void init(){
+    public void init() {
         Kategori k1 = new Kategori(1L, "guld", null);
         Kategori k2 = new Kategori(2L, "silver", null);
         Kategori k3 = new Kategori(3L, "brons", null);
@@ -41,11 +41,11 @@ class KategoriControllerTest {
         when(mockRepo.findById(2L)).thenReturn(Optional.of(k2));
         when(mockRepo.findById(3L)).thenReturn(Optional.of(k3));
 
-        when(mockRepo.findAll()).thenReturn(Arrays.asList(k1,k2,k3));
+        when(mockRepo.findAll()).thenReturn(Arrays.asList(k1, k2, k3));
     }
 
     @Test
-    void getAllKategorier() throws Exception{
+    void getAllKategorier() throws Exception {
         this.mvc.perform(get("/kategorier")).andExpect(status().isOk()).
                 andExpect(content().json("[{\"name\":\"guld\",\"id\":1,\"kunder\":null}," +
                         "{\"name\":\"silver\",\"kunder\":null,\"id\":2}," +
@@ -53,25 +53,25 @@ class KategoriControllerTest {
     }
 
     @Test
-    void addKategori() throws Exception{
+    void addKategori() throws Exception {
         this.mvc.perform(get("/kategorier/add?namn=platinum")).andExpect(status().isOk()).
                 andExpect(content().string(equalTo("Kategori platinum lades till")));
     }
 
     @Test
-    void deleteKategori() throws Exception{
+    void deleteKategori() throws Exception {
         this.mvc.perform(get("/kategorier/delete/1")).andExpect(status().isOk()).
                 andExpect(content().string(equalTo("kategori 1 togs bort")));
     }
 
     @Test
-    void getCategoryById() throws Exception{
+    void getCategoryById() throws Exception {
         this.mvc.perform(get("/kategorier/1")).andExpect(status().isOk()).
                 andExpect(content().json("{\"name\":\"guld\",\"id\":1,\"kunder\":null}"));
     }
 
     @Test
-    void addBookByPOST() throws Exception{
+    void addBookByPOST() throws Exception {
         this.mvc.perform(post("/kategorier/addByPost")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"platinum\",\"id\":4,\"kunder\":null}")).andExpect(status().isOk()).
